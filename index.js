96c0ddd5544b22eb3798d8fc1016bc1898e9a4f5
Scott Ballantyne <scottballantyne@protonmail.com>
2026-04-07 08:27:41 -0500

    putting it all in one file so that people who like that can enjoy the one fileness

--- index.js
+++ index.js
@@ -3,7 +3,22 @@ const crypto = require('crypto');
 const fs = require('fs');
 const ivLength = 16;
 const { Readable, pipeline, buffer } = require('stream');
-const Bufferable = require(path.join(__dirname, 'bufferable'));
+const { Writable } = require('node:stream');
+
+class Bufferable extends Writable {
+  constructor() {
+    super();
+    this.chunks = [];
+  }
+  _write(chunk, encoding, callback) {
+    this.chunks.push(chunk);
+    callback();
+  }
+  get buffer() {
+    return Buffer.concat(this.chunks);
+  }
+}
+
 
 
 class RavenDataFile {
